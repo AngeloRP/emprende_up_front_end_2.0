8f0a7b5a07da00d77a1b7b76854ec9c6f953c356
Angelo\Ursula <angelorp93@gmail.com>
2018-03-23 18:43:55 -0500

    Cambios css

--- src/app/shared/forms/form-content/inputs/input-number/input-number.component.ts
+++ src/app/shared/forms/form-content/inputs/input-number/input-number.component.ts
@@ -1,18 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { InputClassContentPadre } from '../input-content-padre.class';
-
+/**
+ * 
+ * 
+ * @export
+ * @class InputNumberComponent
+ * @extends {InputClassContentPadre}
+ * @implements {OnInit}
+ */
 @Component({
   selector: 'app-input-number',
   templateUrl: './input-number.component.html',
   styleUrls: ['./input-number.component.css']
 })
 export class InputNumberComponent  extends InputClassContentPadre implements OnInit {
-
-  constructor() {
+/**
+ * Creates an instance of InputNumberComponent.
+ * @memberof InputNumberComponent
+ */
+constructor() {
     super();
   }
-
-  ngOnInit() {
+/**
+ * 
+ * 
+ * @memberof InputNumberComponent
+ */
+ngOnInit() {
   }
 
 }
